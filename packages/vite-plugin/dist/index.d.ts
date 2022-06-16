018832a957febb9bdbc7c17247635460b9915237
luoshiheng <luoshiheng1994@gmail.com>
2022-06-16 17:06:30 +0800

    feat: build

---
+++ packages/vite-plugin/dist/index.d.ts
@@ -0,0 +1,3 @@
+import { Plugin } from "vite";
+import { ConfigType } from "@ali-arms-upload-sourcemap/core";
+export default function pluginConfig(config: ConfigType): Plugin;
---
+++ packages/vite-plugin/dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const core_1 = __importDefault(require("@ali-arms-upload-sourcemap/core"));
+function pluginConfig(config) {
+    let outDirFinal;
+    return {
+        name: "@ali-arms-upload-sourcemap/vite-plugin",
+        apply: "build",
+        configResolved(resolvedConfig) {
+            // 获取打包文件夹路径
+            const { root } = resolvedConfig;
+            const { outDir, assetsDir } = resolvedConfig.build;
+            outDirFinal = path_1.default.resolve(root, outDir, assetsDir);
+        },
+        closeBundle() {
+            (0, core_1.default)(config, outDirFinal);
+        }
+    };
+}
+exports.default = pluginConfig;
+//# sourceMappingURL=index.js.map
